f4a5c1d027536430b17cc554cb96aa8d837bf19e
Eugene Makeev <makeev.eugene@gmail.com>
2020-01-13 17:36:35 +0300

    initial commit

---
+++ G-loader/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace G_loader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.GcodeLoad = new System.Windows.Forms.Button();
+            this.FwLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(144, 16);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(129, 39);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Подключить";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(119, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // GcodeLoad
+            // 
+            this.GcodeLoad.Enabled = false;
+            this.GcodeLoad.Location = new System.Drawing.Point(14, 81);
+            this.GcodeLoad.Name = "GcodeLoad";
+            this.GcodeLoad.Size = new System.Drawing.Size(260, 50);
+            this.GcodeLoad.TabIndex = 2;
+            this.GcodeLoad.Text = "Загрузка G-code";
+            this.GcodeLoad.UseVisualStyleBackColor = true;
+            this.GcodeLoad.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FwLoad
+            // 
+            this.FwLoad.Enabled = false;
+            this.FwLoad.Location = new System.Drawing.Point(14, 151);
+            this.FwLoad.Name = "FwLoad";
+            this.FwLoad.Size = new System.Drawing.Size(260, 50);
+            this.FwLoad.TabIndex = 3;
+            this.FwLoad.Text = "Обновление прошивки";
+            this.FwLoad.UseVisualStyleBackColor = true;
+            this.FwLoad.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Порт:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 211);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FwLoad);
+            this.Controls.Add(this.GcodeLoad);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            this.Text = "G-loader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button GcodeLoad;
+        private System.Windows.Forms.Button FwLoad;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ G-loader/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace G_loader
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // populate list of com-ports
+            string[] enableComPorts = SerialPort.GetPortNames();
+            foreach (string port in enableComPorts)
+            {
+                comboBox1.Items.Add(port);
+            }
+
+            if (comboBox1.Items.Count != 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Connect.Text == "Подключить")
+            {
+                port.PortName = comboBox1.GetItemText(comboBox1.SelectedItem);
+                port.BaudRate = 115200;
+                port.Parity = Parity.None;
+                port.StopBits = StopBits.One;
+                port.DataBits = 8;
+                port.Handshake = Handshake.None;
+                port.RtsEnable = false;
+                try
+                {
+                    port.Open();
+                    Connect.Text = "Отключить";
+                    comboBox1.Enabled = false;
+                    FwLoad.Enabled = GcodeLoad.Enabled = true;
+                }
+                catch
+                {
+                    string message = "Порт уже используется другой программой";
+                    string title = "Ошибка подключения";
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                Connect.Text = "Подключить";
+                comboBox1.Enabled = true;
+                FwLoad.Enabled = GcodeLoad.Enabled = false;
+                port.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "All files(*.*)|*.*";
+            if (openDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                string filename = openDialog.FileName;
+                string fileText = System.IO.File.ReadAllText(filename);
+
+                // send file over COM port
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Bin files(*.bin)|*.bin";
+            if (openDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                string filename = openDialog.FileName;
+                string fileText = System.IO.File.ReadAllText(filename);
+
+                // send file over COM port
+            }
+        }
+    }
+}
